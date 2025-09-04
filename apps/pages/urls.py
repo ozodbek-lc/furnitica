8f1e8ebf36f0dc17f5db348637eb53279e1738cb
ozodbek-lc <sherbekovozodbek747@example.com>
2025-09-04 16:15:46 +0500

    Contact qismi yozildi

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'pages'
 
 urlpatterns = [
-    path('',views.pages_home_view,name='home'),
+    path('',views.home_page_view,name='home'),
+    path('contact/',views.contact_page_view,name='contact'),
 ]
--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-def pages_home_view(request):
-    return render(request,'home3.html')
+from apps.pages.forms import ContactForm
+
+
+def home_page_view(request):
+    return render(request, 'home.html')
+
+
+def contact_page_view(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
+            form.result = 1313
+            form.save()
+            return redirect('pages:contact')
+        else:
+            errors = []
+            for key, value in form.errors.items():
+                for error in value:
+                    errors.append(error)
+            context = {
+                "errors": errors
+            }
+            return render(request, 'pages/contact.html', context)
+
+    else:
+        return render(request, 'pages/contact.html')
